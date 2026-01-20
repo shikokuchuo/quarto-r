fb3544273b27ece0164bb96ff68c1902224568eb
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2026-01-20 23:42:41 +0000

    Try tempfile

--- R/quarto.R
+++ R/quarto.R
@@ -157,19 +157,23 @@ quarto_run <- function(
     # for environment variables not being inherited by Rscript subprocess
     # https://github.com/quarto-dev/quarto-r/issues/217
     if (.Platform$OS.type == "windows") {
-      # Create a wrapper script that sets .libPaths() then sources rmd.R
+      # Get Quarto's resource path and read rmd.R
+      paths_output <- system2(quarto_bin, "--paths", stdout = TRUE)
+      resources_path <- paths_output[2]
+      rmd_r_path <- file.path(resources_path, "rmd", "rmd.R")
+      rmd_content <- readLines(rmd_r_path, warn = FALSE)
+
+      # Create .libPaths() call to prepend
       escaped_paths <- gsub("\\", "\\\\", libpaths, fixed = TRUE)
       escaped_paths <- gsub('"', '\\"', escaped_paths, fixed = TRUE)
       paths_str <- paste0('"', escaped_paths, '"', collapse = ", ")
+      libpaths_call <- sprintf('.libPaths(c(%s, .libPaths()))', paths_str)
 
-      wrapper_content <- sprintf(
-        '.libPaths(c(%s, .libPaths()))\nargs <- commandArgs(trailingOnly = FALSE)\nsource(args[length(args)])',
-        paths_str
-      )
+      # Write combined script: .libPaths() + original rmd.R content
       wrapper_file <- tempfile("quarto_libpaths_", fileext = ".R")
-      writeLines(wrapper_content, wrapper_file)
+      writeLines(c(libpaths_call, rmd_content), wrapper_file)
 
-      # Use --file=wrapper to execute our wrapper, which then sources rmd.R
+      # Use --file=wrapper to execute our combined script
       existing_args <- Sys.getenv("QUARTO_KNITR_RSCRIPT_ARGS", "")
       new_args <- paste0("--file=", wrapper_file)
       if (nzchar(existing_args)) {
